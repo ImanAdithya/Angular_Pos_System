f8fb202013b381a2443375a166de0d8eba3b4a69
Iman Adithya <121780063+ImanAdithya@users.noreply.github.com>
2023-09-02 12:58:35 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ItemComponent } from './item/item.component';
+import { PlaceOrderComponent } from './place-order/place-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    DashboardComponent,
+    ItemComponent,
+    PlaceOrderComponent
   ],
   imports: [
     BrowserModule,
